1d358bb9639f2f42e973460edf86f21bea81b9a6
chenstarsQ <1140330611@qq.com>
2018-07-18 16:44:34 +0800

    define hashmap define

--- practice_for_hardway/hashmap.py
+++ practice_for_hardway/hashmap.py
@@ -19,6 +19,7 @@ def hash_key(aMap, key):
     """Given a key this will create a number and then convert it to
     an index for the aMap's buckets."""
     return hash(key) % len(aMap)
+# 是dict如何工作的核心
 
 
 def get_bucket(aMap, key):
