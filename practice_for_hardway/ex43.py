bb5a1654bc63b3ab717e92f9007def69c0a12290
chenstarsQ <1140330611@qq.com>
2018-07-24 18:30:27 +0800

    add
    add

--- practice_for_hardway/ex43.py
+++ practice_for_hardway/ex43.py
@@ -11,20 +11,38 @@ import random
 
 class Scene(object):
     def enter(self):
-        pass
+        print("""this scene is not yet configured.
+            Subclass it and implement enter""")
+        sys.exit(1)
 
 
 class Engine(object):
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.scene_name(next_scene_name)
+
+    # be sure to print out the last scene
+    current_scene.enter()
 
 
 class Death(Scene):
+    quips = [
+        "You died. You kinda suck at this.",
+        "Your mom would be proud...if she were smarter.",
+        "Such a luser.",
+        "I have a small puppy that's better at this."
+    ]
+
     def enter(self):
-        pass
+        print(Death.quips[random.randint(0, len(self, quips)-1)])
+        exit(1)
 
 
 class CentralCorridor(Scene):
@@ -40,3 +58,24 @@ class LaserWeaponArmory(Scene):
 class TheBridge(Scene):
     def enter(self):
         pass
+
+
+class EscapePod(Scene):
+    def enter(self):
+        pass
+
+
+class Map(object):
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
