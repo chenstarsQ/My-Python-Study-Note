49ecadefcc299dee5fda87ba1ffdeba39edc9501
chenstarsQ <1140330611@qq.com>
2018-08-20 08:14:58 +0800

    none

--- practice_for_hardway/ex16.py
+++ practice_for_hardway/ex16.py
@@ -16,7 +16,7 @@ target.truncate()
 
 print("Now I'm going to ask you for three lines.")
 
-line1 = input("line 1: ")
+line1 = input("line 1:")
 line2 = input("line 2: ")
 line3 = input("line 3: ")
 
