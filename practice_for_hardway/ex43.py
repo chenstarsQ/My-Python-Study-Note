414879e4cc9be677ea340288942f11f035e8c5be
chenstarsQ <1140330611@qq.com>
2018-08-22 10:46:57 +0800

    debug

--- practice_for_hardway/ex43.py
+++ practice_for_hardway/ex43.py
@@ -19,7 +19,7 @@ class Engine(object):
     def __init__(self, scene_map):
         self.scene_map = scene_map
 
-    def play(self):`
+    def play(self):
         current_scene = self.scene_map.opening_scene()
         last_scene = self.scene_map.next_scene('finished')
 
