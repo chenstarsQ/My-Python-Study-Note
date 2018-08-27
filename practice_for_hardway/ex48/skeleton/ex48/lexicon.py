67cfd9037b9ffd8d3417b7a5ee8f142cdb0a6c39
chenstarsQ <1140330611@qq.com>
2018-08-27 11:49:19 +0800

    debug

--- practice_for_hardway/ex48/skeleton/ex48/lexicon.py
+++ practice_for_hardway/ex48/skeleton/ex48/lexicon.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Date    : 2018-08-22 14:53:34
 # @Author  : Star (1140330611@qq.com)
-# @Link    : ${link}
+# @Link    : https://github.com/chenstarsQ/My-Python-Study-Note
 # @Version : $Id$
 
 
